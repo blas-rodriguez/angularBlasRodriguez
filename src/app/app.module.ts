fee09ebf46c0d49bc936b2d69cd4ec3134fc6672
blasrodriguez <blasrodriguez934@gmail.com>
2019-10-26 14:49:38 -0300

    otro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PorductComponent } from './components/product.components'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PorductComponent
   ],
   imports: [
     BrowserModule,
